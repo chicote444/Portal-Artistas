36ebdd3c7c257728517d0ca86f16edd7cacd0f63
pedrowslly <163873110+pedrowslly@users.noreply.github.com>
2024-09-04 12:29:58 +0000

    .

--- back/dados.js
+++ back/dados.js
@@ -16,3 +16,23 @@ export const eventos = [
     }
 ]
 
+export const patrocinios = [
+    {
+        "nome": 'Patrocínio Ouro',
+        "descricao": ' Patrocínio principal para o evento Jampa Rock Festival 2024, Logotipo em destaque nos palcos principais e em todo o material promocional.'
+        "beneficios": ' Espaço para estande no festival, Mídia social dedicada e mencões durante o evento',
+        "disponibilidade": ' 3 vagas disponíveis.'
+        "valor": ' R$ 50.000,00'
+    },
+    {
+        "nome": '',
+        "popularidade": '',
+        "descricao": ''
+    },
+    {
+        "nome": '',
+        "popularidade": '',
+        "descricao": ''
+    }
+]
+
